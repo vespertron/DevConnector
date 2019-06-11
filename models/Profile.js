2a4cf8ed5be53caa4360d41c9131b05bec2d9dc5
vespertron <vesper.annstas@gmail.com>
2019-06-11 06:25:47 -0700

    update routes/api/profile.js with create and edit functionality

--- models/Profile.js
+++ models/Profile.js
@@ -19,7 +19,7 @@ const ProfileSchema = new mongoose.Schema({
     required: true
   },
   skills: {
-    type: String,
+    type: Array,
     required: true
   },
   bio: {
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -71,21 +71,18 @@ router.post(
     if (bio) profileFields.bio = bio;
     if (status) profileFields.status = status;
     if (githubusername) profileFields.githubusername = githubusername;
-    if (skills) profileFields.skills = skills;
-    if (youtube) profileFields.youtube = youtube;
-    if (facebook) profileFields.facebook = facebook;
-    if (twitter) profileFields.twitter = twitter;
-    if (instagram) profileFields.instagram = instagram;
-    if (linkedin) profileFields.linkedin = linkedin;
     if (skills) {
       profileFields.skills = skills.split(',').map(skill => skill.trim());
     }
+
+    console.log(profileFields.skills);
 
     //Build social object
     profileFields.social = {};
     if (youtube) profileFields.social.youtube = youtube;
     if (facebook) profileFields.social.facebook = facebook;
     if (twitter) profileFields.social.twitter = twitter;
+    if (linkedin) profileFields.social.linkedin = linkedin;
     if (instagram) profileFields.instagram.instagram = instagram;
 
     try {
@@ -109,9 +106,43 @@ router.post(
       res.json(profile);
     } catch (err) {
       console.error(err.message);
-      res.status(500).send('Server Error on profile');
+      res.status(500).send('Server Error');
     }
   }
 );
+
+// @route   GET api/profile
+// @desc    Get all profiles
+// @access  Public
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route   GET api/profile/user/:user_id
+// @desc    Get profile by user ID
+// @access  Public
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id
+    }).populate('user', ['name', 'avatar']);
+
+    if (!profile) return res.status(400).json({ msg: 'Profile not found' });
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == 'ObjectId') {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
